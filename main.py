0a31e75c7b42dc9c6a1e5a02e85bb1f2e37b55f8
evan-thoms <evanthoms@outlook.com>
2024-06-26 22:26:48 +0200

    radicals working, full character retrieval

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 import pinyin
 import requests
 import re
+import json
 
 app = Flask(__name__)
 
@@ -13,7 +14,6 @@ def contains_chinese_characters(s):
 
 @app.route("/", methods=["POST", "GET"])
 def result():
-    #书
     form_data= request.form.to_dict()
     if 'user_input' in form_data:
         uinput = request.form.to_dict()['user_input']
@@ -32,20 +32,47 @@ def testreturn(uinput):
 definition = ""
 
 charPinyin = ""
-def getPinyin():
-        # 
-        pinput = "running"
-        print(pinput)
 def getCharInfo(uinput):
      #uinput = input("Input a character to get a pinyin and an image")
-     #uinput = "读"
-     url = "http://ccdb.hemiola.com/characters/string/"+uinput+"?fields=kDefinition,kMandarin"
+    #  uinput = "读"
+     url = "http://ccdb.hemiola.com/characters/string/"+uinput+"?fields=kDefinition,kMandarin,kRSKangXi"
      response = requests.get(url, headers={"User-Agent": "XY"})
      definition = response.json()[0]['kDefinition']
+     radNum = response.json()[0]["kRSKangXi"]
+     print(response.json()[0])
+     print(response.json()[0], " response")
+     print("originial rad ", radNum)
+     radNum = radNum.split('.')[0]
+     print(radNum, "sdf")
      charPinyin = pinyin.get(uinput)
-     output = "\nYour character "+uinput+ " is pronounced "+charPinyin+" and means "+definition+ ". "
-     return output
+     rad = getRads(radNum)
+     print(rad)
+     radChar = rad['radical']
+     english = rad['english']
+     radNum = str(radNum)
+     char  = "\nYour character "+uinput+ " is pronounced "+charPinyin+" and means "+definition+ ". "
+     rad = "\nYour character uses radical #"+radNum+": "+radChar+", which means "+english+". "
+     return char + rad
+def getRads(radNumC):
+     print("current number ", radNumC)
+
+     radNumC = int(radNumC)
+     with open("radicals.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+     rad=None
+     for item in data:
+        if item['id'] == radNumC:
+            rad = item
+            break
+    
+    # Debugging output to verify the correct item was found
+     print("Found item:", rad)
+     return rad
+     
+
+     
 
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
+    # getCharInfo(3)
