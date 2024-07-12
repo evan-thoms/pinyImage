f8ddf2af926cf35358896fb48801cae710f1b3f5
evan-thoms <evanthoms@outlook.com>
2024-07-12 00:48:39 -0700

    further react migration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const  [data, setData] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/result', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => setData(data));
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React and Flask</h1>
+      {data && (
+        <>
+          <p>{data.result}</p>
+          <p>{data.connections}</p>
+          <div className="cards">
+            {data.cards.map(card => (
+              <div key={card.id} className="card">
+                <h2>{card.title}</h2>
+                <span className="badge badge-primary">{card.created}</span>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
 export default App;
